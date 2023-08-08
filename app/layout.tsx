6dfba9a83c4070b9c40f2c5498b15c786102640d
mdpabel <mdpabel385@gmail.com>
2023-08-08 11:18:26 -0700

    Added stripe checkout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import "../styles/globals.css";
 import { Playfair_Display } from "next/font/google";
 import { Metadata } from "next";
 
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useUser } from "@/stores/user";
-import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
+// import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { useUser } from "@/stores/user";
+// import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
 
 export const metadata: Metadata = {
   title: "WordPress Securites | Your Source for WordPress Security",
@@ -18,30 +18,30 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createServerComponentClient({
-    cookies,
-  });
+  // const supabase = createServerComponentClient({
+  //   cookies,
+  // });
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession();
 
-  if (session) {
-    useUser.setState({
-      email: session?.user?.email,
-      isLoggedIn: true,
-      userId: session?.user?.id,
-    });
-  }
+  // if (session) {
+  //   useUser.setState({
+  //     email: session?.user?.email,
+  //     isLoggedIn: true,
+  //     userId: session?.user?.id,
+  //   });
+  // }
 
   return (
     <html className="scroll-smooth" suppressHydrationWarning={true} lang="en">
       <body className={playfair.className}>
-        <ClientSideStateInitializer
+        {/* <ClientSideStateInitializer
           email={session?.user?.email ?? ""}
           isLoggedIn={!!session?.user}
           userId={session?.user.id ?? ""}
-        />
+        /> */}
         <main className="">{children}</main>
       </body>
     </html>
