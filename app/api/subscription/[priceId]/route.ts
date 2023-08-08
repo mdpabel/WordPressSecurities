94c58eaaae9de17e8965fe6ee839e70f0fc6ca16
mdpabel <mdpabel385@gmail.com>
2023-08-08 11:16:38 -0700

    Added stripe checkout

--- app/api/subscription/[priceId]/route.ts
+++ app/api/subscription/[priceId]/route.ts
@@ -1,58 +1,58 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { stripe } from "@/utils/stripe";
+// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+// import { NextRequest, NextResponse } from "next/server";
+// import { cookies } from "next/headers";
+// import { stripe } from "@/utils/stripe";
 
-export const GET = async (req: NextRequest, context: any) => {
-  try {
-    const { params } = context;
-    const supabase = createRouteHandlerClient<Database>({ cookies });
+// export const GET = async (req: NextRequest, context: any) => {
+//   try {
+//     const { params } = context;
+//     const supabase = createRouteHandlerClient<Database>({ cookies });
 
-    const { data: profile } = await supabase
-      .from("profile")
-      .select("*")
-      .single();
+//     const { data: profile } = await supabase
+//       .from("profile")
+//       .select("*")
+//       .single();
 
-    if (!profile) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Unauthorized",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
+//     if (!profile) {
+//       return NextResponse.json(
+//         {
+//           success: false,
+//           message: "Unauthorized",
+//         },
+//         {
+//           status: 401,
+//         }
+//       );
+//     }
 
-    const checkoutSession = await stripe.checkout.sessions.create({
-      success_url: process.env.SITE_URL + "/payment-status?status=success",
-      cancel_url: process.env.SITE_URL + "/payment-status?status=cancel",
-      payment_method_types: ["card"],
-      mode: "subscription",
-      customer: profile?.stripe_customer as string,
-      line_items: [
-        {
-          price: params.priceId,
-          quantity: 1,
-        },
-      ],
-    });
+//     const checkoutSession = await stripe.checkout.sessions.create({
+//       success_url: process.env.SITE_URL + "/payment-status?status=success",
+//       cancel_url: process.env.SITE_URL + "/payment-status?status=cancel",
+//       payment_method_types: ["card"],
+//       mode: "subscription",
+//       customer: profile?.stripe_customer as string,
+//       line_items: [
+//         {
+//           price: params.priceId,
+//           quantity: 1,
+//         },
+//       ],
+//     });
 
-    return NextResponse.json({
-      success: true,
-      sessionId: checkoutSession.id,
-    });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      {
-        status: false,
-        message: "Something went wrong",
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-};
+//     return NextResponse.json({
+//       success: true,
+//       sessionId: checkoutSession.id,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       {
+//         status: false,
+//         message: "Something went wrong",
+//       },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// };
