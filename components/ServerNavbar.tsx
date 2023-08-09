7fd869ff90fe09c5ed0b3dad9883ac5a3408f192
mdpabel <mdpabel385@gmail.com>
2023-08-09 04:55:10 -0700

    testing navar server component

--- components/ServerNavbar.tsx
+++ components/ServerNavbar.tsx
@@ -1,21 +1,21 @@
-import { cookies, headers } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useUser } from "@/stores/user";
-import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
+// import { cookies, headers } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { useUser } from "@/stores/user";
+// import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
 
 import Navbar from "./Navbar";
 
-const getSession = async (cookie: string) => {
-  const response = await fetch("http://localhost:3000/api/auth/session", {
-    headers: {
-      cookie,
-    },
-    cache: "no-cache",
-  });
-  const session = await response.json();
+// const getSession = async (cookie: string) => {
+//   const response = await fetch("http://localhost:3000/api/auth/session", {
+//     headers: {
+//       cookie,
+//     },
+//     cache: "no-cache",
+//   });
+//   const session = await response.json();
 
-  return session;
-};
+//   return session;
+// };
 
 const ServerNavbar = async () => {
   // const supabase = createServerComponentClient({
@@ -26,9 +26,9 @@ const ServerNavbar = async () => {
   //   data: { session },
   // } = await supabase.auth.getSession();
 
-  const session = await getSession(headers().get("cookie") ?? "");
+  // const session = await getSession(headers().get("cookie") ?? "");
 
-  console.log(session);
+  // console.log(session);
 
   // if (session) {
   //   useUser.setState({
