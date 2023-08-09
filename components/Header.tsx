fe8cffc49c1c7e14050e2db401e28429ae1b8101
mdpabel <mdpabel385@gmail.com>
2023-08-09 02:00:35 -0700

    testing navar server component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import TopBar from "./TopBar";
-import Navbar from "./Navbar";
+import ServerNavbar from "./ServerNavbar";
 
 const Header = () => {
   return (
     <>
       <TopBar />
-      <Navbar />
+      <ServerNavbar />
     </>
   );
 };
--- components/ServerNavbar.tsx
+++ components/ServerNavbar.tsx
@@ -1,30 +1,44 @@
-// import { cookies } from "next/headers";
-// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-// import { useUser } from "@/stores/user";
-// import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
+import { cookies, headers } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useUser } from "@/stores/user";
+import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
 
-// import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 
-// export const dynamic = "force-dynamic";
+const getSession = async (cookie: string) => {
+  const response = await fetch("http://localhost:3000/api/auth/session", {
+    headers: {
+      cookie,
+    },
+    cache: "no-cache",
+  });
+  const session = await response.json();
 
-// const ServerNavbar = async () => {
-//   const supabase = createServerComponentClient({
-//     cookies,
-//   });
+  return session;
+};
 
-//   const {
-//     data: { session },
-//   } = await supabase.auth.getSession();
+const ServerNavbar = async () => {
+  // const supabase = createServerComponentClient({
+  //   cookies,
+  // });
 
-//   if (session) {
-//     useUser.setState({
-//       email: session?.user?.email,
-//       isLoggedIn: true,
-//       userId: session?.user?.id,
-//     });
-//   }
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession();
 
-//   return <Navbar />;
-// };
+  const session = await getSession(headers().get("cookie") ?? "");
 
-// export default ServerNavbar;
+  console.log(session);
+
+  // if (session) {
+  //   useUser.setState({
+  //     email: session?.user?.email,
+  //     isLoggedIn: true,
+  //     userId: session?.user?.id,
+  //   });
+  // }
+
+  return <Navbar />;
+};
+
+export default ServerNavbar;
