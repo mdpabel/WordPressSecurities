5cce6d2ad9cefb3a3ca9174ae4b14f65da40ca26
mdpabel <mdpabel385@gmail.com>
2023-08-09 00:25:17 -0700

    testing navar server component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,7 +34,28 @@ const navItems = [
   },
 ];
 
-const Navbar = () => {
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useUser } from "@/stores/user";
+import ClientSideStateInitializer from "@/components/ClientSideStateInitializer";
+
+const Navbar = async () => {
+  const supabase = createServerComponentClient({
+    cookies,
+  });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    useUser.setState({
+      email: session?.user?.email,
+      isLoggedIn: true,
+      userId: session?.user?.id,
+    });
+  }
+
   return (
     <ComponentWrapper className="relative bg_primary">
       <nav className="flex items-center justify-between py-4">
@@ -55,16 +76,23 @@ const Navbar = () => {
             </li>
           ))}
 
-          <li>
-            <Link prefetch className={`text-lg font-medium `} href="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link prefetch className={`text-lg font-medium `} href="/login">
-              Login
-            </Link>
-          </li>
+          {session ? (
+            <li>
+              <Link
+                prefetch
+                className={`text-lg font-medium `}
+                href="/dashboard"
+              >
+                Dashboard
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link prefetch className={`text-lg font-medium `} href="/login">
+                Login
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </ComponentWrapper>
