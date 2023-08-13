b6a0cf8f5b89604fa6f24b4a12e63d1e2e97dc0b
mdpabel <mdpabel385@gmail.com>
2023-08-13 05:12:54 -0700

    Updated subscription status

--- app/(authenticatedApp)/manage-subscriptions/Subscription.tsx
+++ app/(authenticatedApp)/manage-subscriptions/Subscription.tsx
@@ -3,6 +3,7 @@ import SubTitle from "../SubTitle";
 import { Subscription } from "@prisma/client";
 import { formateDate } from "@/utils/formateDate";
 import SubscriptionButtons from "./SubscriptionButtons";
+import clsx from "clsx";
 
 const Subscription = ({ subscription }: { subscription: Subscription }) => {
   const interval = subscription.interval_count + " " + subscription.interval;
@@ -17,6 +18,7 @@ const Subscription = ({ subscription }: { subscription: Subscription }) => {
     packageName === "Annual" ? 99 : packageName === "Quarterly" ? 39 : 59;
 
   const formateExpiredDate = formateDate(subscription.current_period_end);
+  const isActive = !!subscription.cancellation_date;
 
   return (
     <div
@@ -29,7 +31,16 @@ const Subscription = ({ subscription }: { subscription: Subscription }) => {
         <SubTitle>{packageName}</SubTitle>
         <h3>
           Expires on {formateExpiredDate} | {price} USD | https://mdpabel.com |
-          <span className="text-green-500 font-bold"> Active</span>
+          <span
+            className={clsx({
+              "font-bold": true,
+              "text-green-500 ": isActive,
+              "text-red-500 ": !isActive,
+            })}
+          >
+            {" "}
+            {isActive ? "Active" : "Cancelled"}
+          </span>
         </h3>
       </div>
       <SubscriptionButtons subscription_id={subscription.subscription_id} />
