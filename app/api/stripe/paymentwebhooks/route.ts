5e07fb902a615b9b57fc930d8ab5894aac77c33b
mdpabel <mdpabel385@gmail.com>
2023-08-16 10:34:14 -0700

    Save payment info in product table

--- app/api/stripe/paymentwebhooks/route.ts
+++ app/api/stripe/paymentwebhooks/route.ts
@@ -5,52 +5,71 @@ import { currentUser } from "@clerk/nextjs";
 import { stripe } from "@/utils/stripe";
 
 export const POST = async (req: NextRequest) => {
-  const user = await currentUser();
-  const sig = headers().get("stripe-signature") as string;
-  const signingSecrete = process.env.STRIPE_WEBHOOKS_SIGNING_SECRETE!;
-  const reqString = await req.text();
+  try {
+    const user = await currentUser();
+    const sig = headers().get("stripe-signature") as string;
+    const signingSecrete = process.env.STRIPE_WEBHOOKS_SIGNING_SECRETE!;
+    const reqString = await req.text();
 
-  const profile = await prisma.user.findFirst({
-    where: {
-      clerkId: user?.id,
-    },
-  });
+    const profile = await prisma.user.findFirst({
+      where: {
+        clerkId: user?.id,
+      },
+    });
 
-  if (!profile) {
+    if (!profile) {
+      return NextResponse.json(
+        {
+          message: "You are not authorized to access this endpoint",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(reqString, sig, signingSecrete);
+    } catch (err: any) {
+      console.log(err);
+      return NextResponse.json(`Webhook Error: ${err.message}`, {
+        status: 400,
+      });
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.completed":
+        const checkoutSessionCompleted: any = event.data.object;
+        await prisma.order.create({
+          data: {
+            amount: checkoutSessionCompleted.amount_total,
+            orderId: checkoutSessionCompleted.id,
+            userId: profile?.id,
+          },
+        });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return NextResponse.json({
+      status: "OK",
+      message: "Webhook POST request received",
+      received: true,
+    });
+  } catch (error: any) {
+    console.log(error);
     return NextResponse.json(
       {
-        message: "You are not authorized to access this endpoint",
+        success: false,
+        message: `Webhook Error: ${error.message}`,
       },
       {
-        status: 401,
+        status: 400,
       }
     );
   }
-
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(reqString, sig, signingSecrete);
-  } catch (err: any) {
-    console.log(err);
-    return NextResponse.json(`Webhook Error: ${err.message}`, {
-      status: 400,
-    });
-  }
-
-  // Handle the event
-  switch (event.type) {
-    case "checkout.session.completed":
-      const checkoutSessionCompleted = event.data.object;
-      console.log(checkoutSessionCompleted);
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  return NextResponse.json({
-    status: "OK",
-    message: "Webhook POST request received",
-    received: true,
-  });
 };
