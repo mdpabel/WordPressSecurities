c4df86761f1d356139330feccc4184c7a3815643
mdpabel <mdpabel385@gmail.com>
2023-09-07 23:38:55 -0700

    Added a contentful webhook

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
 
 export const POST = async (req: NextRequest) => {
   const r = await req.json();
 
-  console.log(r);
+  console.log(r, headers);
 
   await revalidatePath("/guides");
 
   return NextResponse.json({
     success: true,
     req: r,
+    headers,
   });
 };
