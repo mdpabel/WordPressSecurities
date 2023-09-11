3282d4b47e591f03c7c1cc76f95094010afd882a
mdpabel <mdpabel385@gmail.com>
2023-09-11 05:38:59 -0700

    Revalidate route

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
 
 export const GET = async (request: NextRequest) => {
   const path = request.nextUrl.searchParams.get("path");
@@ -24,13 +25,31 @@ export async function POST(request: NextRequest) {
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
 
-export async function PUT(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get("path");
+export async function PUT(res: NextRequest) {
+  const path = res.nextUrl.searchParams.get("path");
+  const headersList = headers();
+  const token = `Bearer ${process.env.REVALIDATE_SECRET_KEY}`;
+  const authToken = headersList.get("authorization");
+
+  if (token != authToken) {
+    return NextResponse.json(
+      {
+        message: "Invalid token",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
 
   if (!path) {
     return NextResponse.json(
-      { message: "Missing path param" },
-      { status: 400 }
+      {
+        message: "No paths",
+      },
+      {
+        status: 412,
+      }
     );
   }
 
