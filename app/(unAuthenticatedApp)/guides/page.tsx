cf5a279760b8b31f36a302e62305d63b33cfe835
mdpabel <mdpabel385@gmail.com>
2023-09-11 09:38:27 -0700

    Added tailwind typographi

--- app/(unAuthenticatedApp)/guides/page.tsx
+++ app/(unAuthenticatedApp)/guides/page.tsx
@@ -8,7 +8,7 @@ import { PostType, getPosts } from "@/lib/posts";
 import { notFound } from "next/navigation";
 
 export const dynamic = "force-static";
-export const revalidate = 100;
+export const revalidate = true;
 
 const Blog = async () => {
   const blogs: PostType[] = await getPosts();
