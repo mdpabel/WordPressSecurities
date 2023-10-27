f5f8631acd038835771421c15b6879b5ade30d72
mdpabel <mdpabel385@gmail.com>
2023-10-28 00:11:50 +0600

    Updated header

--- app/(unAuthenticatedApp)/pricing/page.tsx
+++ app/(unAuthenticatedApp)/pricing/page.tsx
@@ -2,6 +2,7 @@ import ComponentWrapper from "@/components/common/ComponentWrapper";
 import React from "react";
 import FAQ from "@/components/FAQ";
 import PricingTables from "@/components/payment/PricingTables";
+import { SectionTitleWithSubTitle } from "@/components/common/Title";
 
 export const dynamic = "force-static";
 export const revalidate = 86400;
@@ -9,7 +10,14 @@ export const revalidate = 86400;
 const solutions = () => {
   return (
     <ComponentWrapper>
-      <PricingTables />
+      <div className="py-10">
+        <SectionTitleWithSubTitle
+          title="Safely Empower Your Digital Business"
+          subTitle="Lock Down Your Digital Assets - Clearly Defined Subscription Tiers -
+        Engineered for Small to Large-scale Websites"
+        />
+        <PricingTables />
+      </div>
       <FAQ />
     </ComponentWrapper>
   );
