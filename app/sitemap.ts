a63605c3a04266920eab255b534c9c19c7fe835c
MD_Pabel <mdpabel385@gmail.com>
2024-01-06 17:36:01 +0600

    fix: migrate to swell

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,46 +1,45 @@
-import { getServices } from "@/lib/contentful";
+import { getProducts } from '@/lib/swell/product';
 
-const siteUrl =
-  process.env.SITE_URL || "SITE_URLhttps://www.wordpresssecurities.com";
+const siteUrl = process.env.SITE_URL || 'https://www.wordpresssecurities.com';
 
 type SitemapType = Array<{
   url: string;
   lastModified?: string | Date;
   changeFrequency?:
-    | "always"
-    | "hourly"
-    | "daily"
-    | "weekly"
-    | "monthly"
-    | "yearly"
-    | "never";
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never';
   priority?: number;
 }>;
 
 const unAuthenticatedPages = [
-  "",
-  "emergency",
-  "faq",
-  "pricing",
-  "privacy",
-  "privacy",
-  "terms",
+  '',
+  'emergency',
+  'faq',
+  'pricing',
+  'privacy',
+  'privacy',
+  'terms',
 ];
 
 export default async function sitemap() {
   const staticPages: SitemapType = unAuthenticatedPages.map((p) => ({
-    url: siteUrl + "/" + p,
+    url: siteUrl + '/' + p,
     lastModified: new Date(),
-    changeFrequency: "monthly",
+    changeFrequency: 'monthly',
     priority: 1,
   }));
 
-  const solutions = await getServices();
+  const solutions = await getProducts();
 
   const dynamicPages: SitemapType = solutions.map((solution) => ({
-    url: siteUrl + "/solutions/" + solution.slug,
+    url: siteUrl + '/solutions/' + solution.slug,
     lastModified: new Date(),
-    changeFrequency: "monthly",
+    changeFrequency: 'monthly',
     priority: 1,
   }));
 
