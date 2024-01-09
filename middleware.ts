3f4b3b02dcf481d68258866ba354988027f40dcf
MD_Pabel <mdpabel385@gmail.com>
2024-01-09 17:35:27 +0600

    build: webhook

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,60 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs/server';
+import {
+  authMiddleware,
+  redirectToSignIn,
+  clerkClient,
+} from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const publicRoutes = ['/((?!dashboard).*)'];
+const publicRoutes = [
+  '/',
+  '/emergency(.*)',
+  '/guides(.*)',
+  '/solutions(.*)',
+  '/pricing(.*)',
+  '/login(.*)',
+  '/register(.*)',
+  '/about(.*)',
+  '/contact(.*)',
+  '/faq(.*)',
+  '/forget-password(.*)',
+  '/api(.*)',
+];
 
 export default authMiddleware({
   publicRoutes: publicRoutes,
   afterAuth: async (auth, req) => {
+    // Don't need to check auth for public routes
+
     const isAuthRoutes =
       req.nextUrl.pathname.startsWith('/login') ||
       req.nextUrl.pathname.startsWith('/register');
 
     if (auth.userId && isAuthRoutes) {
       return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+
+    if (auth.userId && req.nextUrl.pathname == '/dashboard') {
+      const user = await clerkClient.users.getUser(auth.userId);
+      const privateMetadata = user.privateMetadata as {
+        role: 'admin' | null;
+        isAdmin: boolean;
+      };
+
+      if (privateMetadata.isAdmin) {
+        return NextResponse.redirect(new URL('/admin', req.url));
+      }
+    }
+
+    if (auth.userId && req.nextUrl.pathname == '/admin') {
+      const user = await clerkClient.users.getUser(auth.userId);
+      const privateMetadata = user.privateMetadata as {
+        role: 'admin' | null;
+        isAdmin: boolean;
+      };
+
+      if (!privateMetadata.isAdmin) {
+        return redirectToSignIn();
+      }
     }
 
     if (auth.isPublicRoute) {
