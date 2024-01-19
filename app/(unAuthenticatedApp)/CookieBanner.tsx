b79dbedd565c92253697ba74696fd9679011f665
MD_Pabel <mdpabel385@gmail.com>
2024-01-19 14:32:45 +0600

    fix:lazy load cookie banner

--- app/(unAuthenticatedApp)/CookieBanner.tsx
+++ app/(unAuthenticatedApp)/CookieBanner.tsx
@@ -2,7 +2,10 @@ import Script from 'next/script';
 
 const CookieBanner = () => {
   return (
-    <Script src='https://cdn-cookieyes.com/client_data/d58925124e7c7f1749844dac/script.js' />
+    <Script
+      src='https://cdn-cookieyes.com/client_data/d58925124e7c7f1749844dac/script.js'
+      strategy='lazyOnload'
+    />
   );
 };
 
